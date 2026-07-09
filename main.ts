a350a145a15ecd7822b90218589cb91938c1a341
Getta4 <harharpuyo@gmail.com>
2026-07-09 14:31:38 +0900

    修正

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
-input.onPinPressed(TouchPin.P0, function () {
-    music.play(music.createSoundExpression(WaveShape.Sine, 500, 500, 255, 0, 50, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+let i = 0
+let set_min = 0
+function 点灯_分 () {
+    i = 0
+    for (let index = 0; index < set_min; index++) {
+        if (4 < i) {
+            led.plot(i - 5, 1)
+        } else {
+            led.plot(i, 0)
+        }
+        i += 1
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    set_min += 1
+    点灯_分()
 })
 basic.forever(function () {
 	
